21057dacf3f52d42439605794202e46e7b325455
Jethro <jmarasigan@77global.biz>
2025-03-07 13:28:13 +0800

    added forms and adjust css styling

--- calculator/forms.py
+++ calculator/forms.py
@@ -1,4 +1,17 @@
 from django import forms
 
+ACTIVITY_LEVEL = (
+	(1.2, 'Sedentary (little to no exercise)'),
+	(1.375, 'Lightly active (light exercise/sports 1-3 days a week)'),
+	(1.55, 'Moderately active (moderate exercise 3-5 days a week)'),
+	(1.725, 'Very active (hard exercise 6-7 days a week)'),
+	(1.9, 'Super active (very hard exercise, physical job, or training twice a day)'),
+)
+
 class CalculatorTdee(forms.Form):
-	gender = forms.CharField(max_length=20,required=True)
+	gender = forms.ChoiceField(widget=forms.RadioSelect,
+							choices=[('Male','Male'),('Female','Female')],)
+	age = forms.IntegerField()
+	weight = forms.FloatField()
+	height = forms.FloatField()
+	activity_level = forms.ChoiceField(choices=ACTIVITY_LEVEL)
--- calculator/views.py
+++ calculator/views.py
@@ -1,20 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CalculatorTdee
 
-
-
-ACTIVITY_LEVEL = {
-	'sedentary': 1.2,
-	'lightly':1.375,
-	'moderate':1.725,
-	'very':1.9
-}
 # Create your views here.
 def calculate_tdee(gender:str, age:int, weight:float, height:float, activity_level:str, body_fat:float):
-	act_level = activity_level.split()[0].lower()
-	tdee = ((10 * weight + 6.25 * height - 5 * age) + (5 if gender == 'male' else -151)) * ACTIVITY_LEVEL[act_level]
+	tdee = ((10 * weight + 6.25 * height - 5 * age) + (5 if gender == 'male' else -151)) * float(activity_level)
 	return tdee
 
 def index(request):
-	return render(request,'calculator/index.html',{
-		'tdee':calculate_tdee(gender='male',age=28,weight=87,height=169,activity_level='Moderate active',body_fat=None)
-	})
+	if request.method == 'POST':
+		form = CalculatorTdee(request.POST)
+		if form.is_valid():
+			gender = form.cleaned_data['gender']
+			age = form.cleaned_data['age']
+			weight = form.cleaned_data['weight']
+			height = form.cleaned_data['height']
+			activity_level = form.cleaned_data['activity_level']
+			tdee = calculate_tdee(gender=gender,age=age,weight=weight,height=height,activity_level=activity_level,body_fat=None)
+			return render(request,'calculator/index.html',{'tdee':tdee})
+	else:
+		form = CalculatorTdee()
+
+	context = {'form': form}
+	return render(request,'calculator/index.html', context)
